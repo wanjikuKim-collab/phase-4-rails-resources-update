b9bf3ec04b26798b78c4f8db501542a6bd5d71f3
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-14 01:07:39 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,34 @@ class BirdsController < ApplicationController
     end
   end
 
+  # PATCH /birds/:id
+  def update
+    #find
+    bird = Bird.find_by(id: params[:id])
+
+    if bird
+      bird.update(bird_params)
+      render json: bird, status: :accepted
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird. find_by(id: params[:index])
+
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end    
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
